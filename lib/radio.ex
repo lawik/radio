2bea1a652b9f9021083d7ea80465c4750fec6f86
Lars Wikman <lars@underjord.io>
2022-11-18 10:04:17 +0100

    Start radio project

---
+++ lib/radio.ex
@@ -0,0 +1,18 @@
+defmodule Radio do
+  @moduledoc """
+  Documentation for Radio.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Radio.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
